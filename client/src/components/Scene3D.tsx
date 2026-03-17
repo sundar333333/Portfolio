20c2393f0520a9a7d7c0a0e79224d1229cea4c43
sundar333333 <sundarvijay303@gmail.com>
2026-03-18 00:06:39 +0530

    Fix video fill screen

--- client/src/components/Scene3D.tsx
+++ client/src/components/Scene3D.tsx
@@ -234,7 +234,7 @@ function VintageTV({ hoveredText, onClick, isVideoPlaying, isMuted, visible, gli
         ctx.translate(canvas.width / 2, canvas.height / 2);
         ctx.rotate(-Math.PI / 2);
         ctx.scale(1, -1);
-        ctx.drawImage(video, -canvas.height / 2, -canvas.width / 2, canvas.height, canvas.width);
+        ctx.drawImage(video, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
         ctx.restore();
         videoTextureRef.current.needsUpdate = true;
         mat.map = videoTextureRef.current;
